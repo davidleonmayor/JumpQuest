73cb8367411a734162f93c6e2729bab2e2207c7e
david leon <jdleonch@gmail.com>
2023-11-19 17:17:25 -0500

    Remove .pyc files, dead player funcionality

--- constants/player.py
+++ constants/player.py
@@ -7,9 +7,11 @@ CHARACTER_SCALING = 1
 # can player jump
 CAN_PLAYER_JUMP = True
 # player movement speed
-PLAYER_MOVEMENT_SPEED = 8
+PLAYER_MOVEMENT_SPEED = 8*2.8
 # player jump speed
 PLAYER_JUMP_SPEED = 22
 # initial player position
 PLAYER_INITIAL_POSITION_X = 500
 PLAYER_INITIAL_POSITION_Y = 128
+
+IS_LIFE = True
